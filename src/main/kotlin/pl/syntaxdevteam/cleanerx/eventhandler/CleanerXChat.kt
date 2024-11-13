b48b659b4d0a0c5af4b51b50a1fc8ae9c3ea78f5
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-11-13 23:01:34 +0100

    Updated CleanerXChat class to block sending links

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
@@ -5,13 +5,28 @@ import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.plugin.java.JavaPlugin
 import pl.syntaxdevteam.cleanerx.base.WordFilter
 
-class CleanerXChat(private val wordFilter: WordFilter, private val fullCensorship: Boolean) : Listener {
+class CleanerXChat(
+    private val plugin: JavaPlugin,
+    private val wordFilter: WordFilter,
+    private val fullCensorship: Boolean
+) : Listener {
+
+    private val linkRegex = "(https?://\\S+|www\\.\\S+)".toRegex()
+    private val blockLinks: Boolean = plugin.config.getBoolean("block-links")
 
     @EventHandler
     fun onChat(event: AsyncChatEvent) {
         val message: String = (event.originalMessage() as TextComponent).content()
+
+        if (blockLinks && linkRegex.containsMatchIn(message)) {
+            event.isCancelled = true
+            event.player.sendRichMessage("<red>>Sharing links in the chat is not allowed on this server!")
+            return
+        }
+
         if (wordFilter.containsBannedWord(message)) {
             event.message(Component.text(wordFilter.censorMessage(message, fullCensorship)))
         }
