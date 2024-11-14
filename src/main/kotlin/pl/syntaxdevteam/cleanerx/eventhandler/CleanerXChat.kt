6fad48ced6637d156ab49f7c7e35192c912865cc
SyntaxDevTeam <syntaxerr85@gmail.com>
2024-11-14 18:59:28 +0100

    Minor corrections

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
@@ -10,7 +10,7 @@ import pl.syntaxdevteam.cleanerx.base.WordFilter
 import pl.syntaxdevteam.cleanerx.base.SwearCounter
 
 class CleanerXChat(
-    plugin: CleanerX,
+    private var plugin: CleanerX,
     private val wordFilter: WordFilter,
     private val fullCensorship: Boolean,
     private val swearCounter: SwearCounter
@@ -18,21 +18,23 @@ class CleanerXChat(
 
     private val linkRegex = "(https?://\\S+|www\\.\\S+|\\b\\w+\\.(com|net|org|pl|io|co|gov|edu|info|biz)\\b)".toRegex()
     private val blockLinks = plugin.config.getBoolean("block-links")
+    private val noLink: String = plugin.config.getString("message.no-link") ?: "<red>>Sharing links in the chat is not allowed on this server!"
 
     @EventHandler
     fun onChat(event: AsyncChatEvent) {
-        val player = event.player
-        val message = (event.originalMessage() as TextComponent).content()
+        val message: String = (event.originalMessage() as TextComponent).content()
 
         if (blockLinks && linkRegex.containsMatchIn(message)) {
             event.isCancelled = true
-            player.sendMessage(Component.text("§cNie możesz wysyłać linków na czacie!"))
+            event.player.sendRichMessage(noLink)
             return
         }
 
         if (wordFilter.containsBannedWord(message)) {
             event.message(Component.text(wordFilter.censorMessage(message, fullCensorship)))
-            swearCounter.incrementSwearCount(player)
+            if(blockLinks) {
+                swearCounter.incrementSwearCount(event.player)
+            }
         }
     }
 }
