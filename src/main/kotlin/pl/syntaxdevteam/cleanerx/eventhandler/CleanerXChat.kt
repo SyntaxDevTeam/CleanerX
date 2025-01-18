208d0d02655ea854dac3c3c56a130156838545e5
SyntaxDevTeam <syntaxerr85@gmail.com>
2025-01-18 06:20:40 +0100

    Minor corrections

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
@@ -17,10 +17,10 @@ class CleanerXChat(
     private val swearCounter: SwearCounter
 ) : Listener {
 
-    private val linkRegex = "(https?://\\S+|www\\.\\S+|\\b\\w+\\.(com|net|org|pl|io|co|gov|edu|info|biz|ru|uk|us|de|fr|cn|es|it|au|nl|ca|in|jp|se|ch|br|za|pt|tv|me|xyz|tech|online|store|site|live|app|io|ai|dev|ly|digital|agency|solutions|global|world|studio|cloud|media|network|works)\\b)".toRegex()
+    private val linkRegex = "(https?://\\S+|www\\.\\S+|\\b\\w+\\.(com|net|org|pl|co|gov|edu|info|biz|ru|uk|us|de|fr|cn|es|it|au|nl|ca|in|jp|se|ch|br|za|pt|tv|me|xyz|tech|online|store|site|live|app|io|ai|dev|ly|digital|agency|solutions|global|world|studio|cloud|media|network|works)\\b)".toRegex()
     private val blockLinks = plugin.config.getBoolean("block-links")
     private val usePunishment = plugin.config.getBoolean("use-punishment")
-    private val noLink: String = plugin.config.getString("message.no-link") ?: "<red>>Sharing links in the chat is not allowed on this server!"
+    private val noLink: String = plugin.config.getString("message.no-link") ?: "<red>Sharing links in the chat is not allowed on this server!"
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     fun onChat(event: AsyncChatEvent) {
