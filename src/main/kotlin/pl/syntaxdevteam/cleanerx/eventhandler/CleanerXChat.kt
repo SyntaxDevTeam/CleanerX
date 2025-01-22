c3a7ed3ab639ebc6dfa43f9dfb2e4c58ae79fa60
SyntaxDevTeam <syntaxerr85@gmail.com>
2025-01-22 22:34:58 +0100

    Completely change the approach to handling censored messages. Minimizing the risk of conflict with another plugin

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/eventhandler/CleanerXChat.kt
@@ -3,6 +3,7 @@ package pl.syntaxdevteam.cleanerx.eventhandler
 import io.papermc.paper.event.player.AsyncChatEvent
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.TextComponent
+import org.bukkit.Bukkit
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
@@ -22,14 +23,15 @@ class CleanerXChat(
     private val usePunishment = plugin.config.getBoolean("use-punishment")
     private val noLink: String = plugin.config.getString("message.no-link") ?: "<red>Sharing links in the chat is not allowed on this server!"
 
-    @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     fun onChat(event: AsyncChatEvent) {
         try {
+            val player = event.player
             val message: String = (event.originalMessage() as? TextComponent)?.content() ?: return
 
             if (blockLinks && linkRegex.containsMatchIn(message)) {
                 event.isCancelled = true
-                event.player.sendRichMessage(noLink)
+                player.sendRichMessage(noLink)
                 return
             }
 
@@ -41,16 +43,26 @@ class CleanerXChat(
                 }
             }
 
-            if (swearWordCount > 0) {
-                event.message(Component.text(wordFilter.censorMessage(message, fullCensorship)))
-                if(usePunishment){
-                    swearCounter.incrementSwearCount(event.player, swearWordCount)
-                }
+            val censoredMessage = if (swearWordCount > 0) {
+                wordFilter.censorMessage(message, fullCensorship)
+            } else {
+                message
             }
+
+            val component = Component.text("${player.name}: $censoredMessage")
+
+            Bukkit.getOnlinePlayers().forEach { recipient ->
+                recipient.sendMessage(component)
+            }
+
+            if (swearWordCount > 0 && usePunishment) {
+                swearCounter.incrementSwearCount(player, swearWordCount)
+            }
+
+            event.isCancelled = true
         } catch (e: Exception) {
-            plugin.logger.severe("Error in onChat, report it urgently to the plugin author with the message: ${e.message}")
+            plugin.logger.severe("Error in onChat: ${e.message}")
             e.printStackTrace()
         }
     }
-
 }
