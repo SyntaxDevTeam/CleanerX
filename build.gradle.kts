40304e97d8d6152b2896fe0c5c0912b39d044b1b
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-16 20:53:40 +0100

    Updating and completing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "pl.syntaxdevteam"
-version = "1.3.2-DEV"
+version = "1.4.0"
 description = "A sophisticated plugin designed to filter and replace inappropriate language with censored alternatives or remove it entirely, ensuring a clean and respectful gaming environment."
 
 repositories {
@@ -20,8 +20,13 @@ repositories {
 dependencies {
     compileOnly("dev.folia:folia-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("org.eclipse.aether:aether-api:1.1.0")
-    compileOnly("org.yaml:snakeyaml:2.3")
+    compileOnly("org.yaml:snakeyaml:2.4")
     compileOnly("com.google.code.gson:gson:2.12.1")
+    compileOnly("net.kyori:adventure-text-serializer-legacy:4.18.0")
+    compileOnly("net.kyori:adventure-text-minimessage:4.18.0")
+    compileOnly("net.kyori:adventure-text-serializer-gson:4.18.0")
+    compileOnly("net.kyori:adventure-text-serializer-plain:4.18.0")
+    compileOnly("net.kyori:adventure-text-serializer-ansi:4.18.0")
 }
 
 val targetJavaVersion = 21
