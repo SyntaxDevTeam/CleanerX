0ab288f144cdf81e280dc85f182a719f3f5958c4
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-16 20:59:10 +0100

    The logging system in the server console has been rebuilt to the latest version

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/common/Logger.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/common/Logger.kt
@@ -1,40 +1,50 @@
 package pl.syntaxdevteam.cleanerx.common
 
-import io.papermc.paper.plugin.configuration.PluginMeta
 import org.bukkit.Bukkit
 import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.NamedTextColor
-import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.minimessage.MiniMessage
+import pl.syntaxdevteam.cleanerx.CleanerX
 
-@Suppress("UnstableApiUsage", "unused")
-class Logger(pluginMetas: PluginMeta, private val debugMode: Boolean) {
-    private val plName = pluginMetas.name
-    private val plVer = pluginMetas.version
+@Suppress("UnstableApiUsage")
+class Logger(plugin: CleanerX, private val debugMode: Boolean) {
+    private val plName = plugin.pluginMeta.name
+    private val plVer = plugin.pluginMeta.version
+    private val serverVersion = plugin.server.version
+    private val serverName = plugin.server.name
 
     private fun clear(s: String?) {
-        Bukkit.getConsoleSender().sendMessage(LegacyComponentSerializer.legacyAmpersand().deserialize(s!!))
+        Bukkit.getConsoleSender().sendMessage(MiniMessage.miniMessage().deserialize(s!!))
     }
 
     fun success(s: String) {
-        Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] ").color(NamedTextColor.GREEN).append(Component.text(s).color(NamedTextColor.GREEN).decorate(TextDecoration.BOLD)))
+        val miniMessage = MiniMessage.miniMessage()
+        val component = miniMessage.deserialize("<green><bold>[$plName]</bold> $s")
+        Bukkit.getConsoleSender().sendMessage(component)
     }
 
     fun info(s: String) {
-        Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] $s"))
+        val miniMessage = MiniMessage.miniMessage()
+        val component = miniMessage.deserialize("[$plName] $s")
+        Bukkit.getConsoleSender().sendMessage(component)
     }
 
     fun warning(s: String) {
-        Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] ").color(NamedTextColor.GOLD).append(Component.text(s).color(NamedTextColor.GOLD)))
+        val miniMessage = MiniMessage.miniMessage()
+        val component = miniMessage.deserialize("<gold>[$plName] $s")
+        Bukkit.getConsoleSender().sendMessage(component)
     }
 
     fun err(s: String) {
-        Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] ").color(NamedTextColor.RED).append(Component.text(s).color(NamedTextColor.RED)))
+        val miniMessage = MiniMessage.miniMessage()
+        val component = miniMessage.deserialize("<red>[$plName] $s")
+        Bukkit.getConsoleSender().sendMessage(component)
     }
 
     fun severe(s: String) {
-        Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] ").color(NamedTextColor.RED).append(Component.text(s).color(NamedTextColor.RED).decorate(TextDecoration.BOLD)))
+        val miniMessage = MiniMessage.miniMessage()
+        val component = miniMessage.deserialize("<red><bold>[$plName]</bold> $s")
+        Bukkit.getConsoleSender().sendMessage(component)
     }
 
     fun log(s: String) {
@@ -43,28 +53,33 @@ class Logger(pluginMetas: PluginMeta, private val debugMode: Boolean) {
         Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] ").append(component))
     }
 
+    fun clearLog(component: Component) {
+        val coloredMessage = LegacyComponentSerializer.legacySection().serialize(component)
+        Bukkit.getConsoleSender().sendMessage("[$plName] $coloredMessage")
+    }
+
     fun debug(s: String) {
         if (debugMode) {
-            Bukkit.getConsoleSender().sendMessage(Component.text("[$plName] [DEBUG] ").color(NamedTextColor.YELLOW).append(Component.text(s).color(NamedTextColor.YELLOW).decorate(
-                TextDecoration.BOLD)))
+            val miniMessage = MiniMessage.miniMessage()
+            val component = miniMessage.deserialize("<yellow><bold>[$plName] [DEBUG]</bold> $s")
+            Bukkit.getConsoleSender().sendMessage(component)
         }
     }
 
     fun pluginStart(pluginsByPriority: List<Pair<String, String>>) {
         clear("")
-        clear("")
-        clear("&9    __                   __        ___            ")
-        clear("&9   (_      _  |_  _     |  \\  _     |   _  _   _  ")
-        clear("&9   __) \\/ | ) |_ (_| )( |__/ (- \\/  |  (- (_| ||| ")
-        clear("&9       /                                          ")
-        clear("&9           ... is proud to present and enable:")
-        clear("&9                     &f * &f&l${plName} v${plVer}")
+        clear("<blue>    __                   __        ___            ")
+        clear("<blue>   (_      _  |_  _     |  \\  _     |   _  _   _  ")
+        clear("<blue>   __) \\/ | ) |_ (_| )( |__/ (- \\/  |  (- (_| ||| ")
+        clear("<blue>       /                                          ")
+        clear("<blue>           ... is proud to present and enable:")
+        clear("<blue>                     <white> * <white><bold>${plName} v${plVer}")
         for ((pluginName, pluginVersion) in pluginsByPriority) {
-            clear("&9                     &f * $pluginName v$pluginVersion")
+            clear("<blue>                     <white> * $pluginName v$pluginVersion")
         }
-        clear("&9                 utilizing all the optimizations of your server engine!         ")
+        clear("<blue>                 utilizing all the optimizations of your server $serverName $serverVersion!         ")
         clear("")
-        clear("&a    Join our Discord! &9&lhttps://discord.gg/Zk6mxv7eMh")
+        clear("<green>    Join our Discord! <blue><bold>https://discord.gg/Zk6mxv7eMh")
         clear("")
         clear("")
     }
