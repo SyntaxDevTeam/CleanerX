1a058c63d62743ae9a0ed3f59217380868a2768a
SyntaxDevTeam <wieszczy85@gmail.com>
2025-02-18 17:27:11 +0100

    Hotfixed permission

--- src/main/kotlin/pl/syntaxdevteam/cleanerx/commands/CleanerXCommand.kt
+++ src/main/kotlin/pl/syntaxdevteam/cleanerx/commands/CleanerXCommand.kt
@@ -11,12 +11,7 @@ class CleanerXCommand(private val plugin: CleanerX) : BasicCommand {
     private val mH = plugin.messageHandler
 
     override fun execute(@NotNull stack: CommandSourceStack, @NotNull args: Array<String>) {
-        if (!stack.sender.hasPermission("cleanerx.cmd.help") ||
-            !stack.sender.hasPermission("cleanerx.cmd.version") ||
-            !stack.sender.hasPermission("cleanerx.cmd.reload") ||
-            !stack.sender.hasPermission("cleanerx.help") ||
-            !stack.sender.hasPermission("cleanerx.version") ||
-            !stack.sender.hasPermission("cleanerx.reload")) {
+        if (!stack.sender.hasPermission("cleanerx.cmd.crx")) {
             stack.sender.sendMessage(mH.getMessage("error", "no_permission"))
             return
         }
@@ -25,24 +20,25 @@ class CleanerXCommand(private val plugin: CleanerX) : BasicCommand {
         if (args.isNotEmpty()) {
             when {
                 args[0].equals("help", ignoreCase = true) -> {
-                    if (stack.sender.hasPermission("cleanerx.cmd.help") || stack.sender.hasPermission("cleanerx.help")) {
+
                         stack.sender.sendMessage(mH.miniMessageFormat(" <gray>+-------------------------------------------------\n" +
                                 " |\n" +
-                                " |  <gold>Available commands for " + pluginMeta.name + ":\n<gray>#\n" +
+                                " |  <gold>Available commands for " + pluginMeta.name + ":\n" +
+                                " <gray>|\n" +
                                 " |  <gold>/crx help <gray>- <white>Displays this prompt.<gray>\n" +
-                                " <gray>|  <gold>/blacklistx <add/remove/list> <word> <gray>- <white>Adds a word to the blacklist.\n" +
-                                " <gray>|  <gold>/whitelistx <add/remove/list> <word> <gray>- <white>Adds a word to the whitelist.\n" +
+                                " <gray>|  <gold>/blacklistx <add/remove/list> <word> \n" +
+                                " <gray>|                      <gray>- <white>Adds a word to the blacklist.\n" +
+                                " <gray>|  <gold>/whitelistx <add/remove/list> <word> \n" +
+                                " <gray>|                      <gray>- <white>Adds a word to the whitelist.\n" +
                                 " <gray>|  <gold>/cleanx <gray>- <white>Clears the chat window.\n" +
-                                " <gray>|  <gold>/crx version <gray>- <white>Shows plugin info. \n<gray>" +
-                                " |  <gold>/crx reload <gray>- <white>Reloads the configuration file\n" +
+                                " <gray>|  <gold>/crx version <gray>- <white>Shows plugin info. \n" +
+                                " <gray>|  <gold>/crx reload <gray>- <white>Reloads the configuration file\n" +
                                 " |\n" +
                                 " <gray>|\n<gray>+-------------------------------------------------"))
-                    } else {
-                        stack.sender.sendMessage(mH.getMessage("error", "no_permission"))
-                    }
+
                 }
                 args[0].equals("version", ignoreCase = true) -> {
-                    if (stack.sender.hasPermission("cleanerx.cmd.version") || stack.sender.hasPermission("cleanerx.version")) {
+
                     stack.sender.sendMessage(mH.miniMessageFormat("\n<gray>-------------------------------------------------\n" +
                             " <gray>|\n" +
                             " <gray>|   <gold>→ <bold>" + pluginMeta.name + "</bold> ←\n" +
@@ -51,27 +47,27 @@ class CleanerXCommand(private val plugin: CleanerX) : BasicCommand {
                             " <gray>|   <white>Version: <bold><gold>" + pluginMeta.version + "</gold></bold>\n" +
                             " <gray>|" +
                             "\n-------------------------------------------------"))
-                } else {
-                        stack.sender.sendMessage(mH.getMessage("error", "no_permission"))
-                    }
+
                 }
                 args[0].equals("reload", ignoreCase = true) -> {
-                    if (stack.sender.hasPermission("cleanerx.cmd.reload") || stack.sender.hasPermission("cleanerx.reload")) {
+
                         plugin.restartMyTask()
                         stack.sender.sendMessage(mH.getMessage("reload", "success"))
-                    } else {
-                        stack.sender.sendMessage(mH.getMessage("error", "no_permission"))
-                    }
+
                 }
                 else -> {
                     stack.sender.sendMessage(mH.getMessage("error", "unknown_command"))
                 }
             }
         } else {
-            stack.sender.sendMessage("<green>Type /crx help to check available commands")
+            stack.sender.sendMessage(mH.getMessage("error", "unknown_command"))
         }
     }
     override fun suggest(@NotNull stack: CommandSourceStack, @NotNull args: Array<String>): List<String> {
+        if (!stack.sender.hasPermission("cleanerx.cmd.crx")) {
+            stack.sender.sendMessage(mH.getMessage("error", "no_permission"))
+            return emptyList()
+        }
         return when (args.size) {
             1 -> listOf("help", "version", "reload")
             else -> emptyList()
